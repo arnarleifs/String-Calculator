46959f8396b046cbaae6b9b9cdf9e6d46c2c4728
Arnar <arnarleifs@github.com>
2015-10-13 11:09:42 +0000

    Test with negative numbers passed

--- src/main/java/is/ru/stringcalculator/StringCalculator.java
+++ src/main/java/is/ru/stringcalculator/StringCalculator.java
@@ -1,15 +1,30 @@
 package is.ru.stringcalculator;
 import java.util.ArrayList;
 import java.lang.*;
+import java.util.InputMismatchException;
 
 public class StringCalculator {
 	public StringCalculator() {
 
 	}
+
+	private static String getNegativeValues(String numbers) {
+		String negativeString = "";
+		for(int i = 0; i < numbers.length(); i++) {
+			if(numbers.charAt(i) == '-') {
+				negativeString += numbers.substring(i, i + 2) + ",";
+			}
+		}
+		return negativeString.substring(0, negativeString.length() - 1);
+	}
+
 	public static int Add(String numbers) {
 		if(numbers.isEmpty()) {
 			return 0;	
 		} else {
+			if(numbers.contains("-")) {
+				throw new InputMismatchException("Negatives not allowed: " + getNegativeValues(numbers));
+			}
 			String optionalDelimiter = ",|\n";
 			if(numbers.charAt(0) == '/') {
 				optionalDelimiter +=  "|" + numbers.substring(2,3);
@@ -17,8 +32,8 @@ public class StringCalculator {
 			}
 			int sum = 0;
 			String[] numberArray = numbers.split(optionalDelimiter);
-			for(int i = 0; i < numberArray.length; i++) {
-				sum += toInt(numberArray[i]);
+			for(String number : numberArray) {
+				sum += toInt(number);
 			}
 			return sum;
 		}
--- src/test/java/is/ru/stringcalculator/StringCalculatorJUnit.java
+++ src/test/java/is/ru/stringcalculator/StringCalculatorJUnit.java
@@ -1,6 +1,7 @@
 package is.ru.stringcalculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import java.util.InputMismatchException;
 
 // A template class for doing jUnit tests in Java
 public class StringCalculatorJUnit {
@@ -48,5 +49,17 @@ public class StringCalculatorJUnit {
 		int returnValue = calculator.Add("//;\n1;2");
 		assertEquals(3, returnValue);
 		System.out.println("TestAddWithDifferentDelimiter passed!");
+	}
+
+	@Test
+	public void TestAddWithNegativeNumbers() {
+		String message = "";
+		try {
+			int returnValue = calculator.Add("-1, -3, 2");	
+		} catch(InputMismatchException ex) {
+			message = ex.getMessage();
+		}
+		assertEquals(message, "Negatives not allowed: -1,-3");
+		System.out.println("TestAddWithNegativeNumbers passed!");
 	}
 }
